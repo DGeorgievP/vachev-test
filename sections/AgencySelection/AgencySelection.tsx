d4026609f5861acbb0081cae708fee1c73d120d7
DGeorgievP <dimitargeorgievpavlov@gmail.com>
2024-04-24 18:10:52 +0300

    WIP

--- sections/AgencySelection/AgencySelection.tsx
+++ sections/AgencySelection/AgencySelection.tsx
@@ -1,4 +1,10 @@
-import { StyledImageContainer, StyledContainer } from "../Hero/elements";
+import {
+  StyledImageContainer,
+  StyledContainer,
+  StyledTextContainer,
+  StyledDescription,
+  StyledTitle,
+} from "./elements.tsx";
 import Image from "next/image";
 import React from "react";
 
@@ -11,15 +17,23 @@ interface AgencySelectionProps {
 export const AgencySelection: React.FC<AgencySelectionProps> = ({
   title,
   heading,
-  image
+  image,
 }) => {
   return (
     <StyledContainer>
       <StyledImageContainer>
-        <Image layout="responsive" src={image.src} alt={image.alt} width={image.width} height={image.height} />
+        <Image
+          layout="responsive"
+          src={image.src}
+          alt={image.alt}
+          width={image.width}
+          height={image.height}
+        />
       </StyledImageContainer>
-      <h1>{title}</h1>
-      <h2>{heading}</h2>
+      <StyledTextContainer>
+        <StyledTitle>{title}</StyledTitle>
+        <StyledDescription>{heading}</StyledDescription>
+      </StyledTextContainer>
     </StyledContainer>
   );
 };
